df30c856d23857a7bdc7e2f08edf177a5b1ea398
Kat <128542006+HowToLearnCIn24hours@users.noreply.github.com>
2023-06-26 15:55:46 +0000

    upload python analysis code to project repo

---
+++ analysis code.py
@@ -0,0 +1,26 @@
+import mysql.connector
+from mysql.connector import errorcode
+connection = mysql.connector.connect(user='root', password='h:DcY67Nf?', host='localhost', database='lgbt_survey_db')
+cursor = connection.cursor()
+query = ("SELECT maintable.row_id, country.CountryCode, subset.subset, questioncode.question_code, questioncode.question_label, maintable.answer, maintable.percentage FROM maintable, country, questioncode, subset")
+try:
+    cursor.execute(query)
+    #change the password of database because this is going to be public (create new user?)
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+       print("Database does not exist")
+    else:
+        print(err)
+
+import pandas as pd
+import numpy as np
+"""
+import matplotlib as plt
+#import seaborn as sns
+plt.style.use('ggplot')
+pd.set_option('display.max_colwidth', None) #Dispaly full questions
+pd.set_option('max_columns', 200)
+"""
+query.shape
