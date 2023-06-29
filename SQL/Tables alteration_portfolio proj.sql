f466901ad62bf841cf5c8c06a91e44aa1cf79879
Kat <128542006+HowToLearnCIn24hours@users.noreply.github.com>
2023-06-29 20:02:45 +0000

    Update Tables alteration_portfolio proj.sql

--- SQL/Tables alteration_portfolio proj.sql
+++ SQL/Tables alteration_portfolio proj.sql
@@ -1,3 +1,4 @@
+#Tables alternation
 ALTER TABLE maintable
 DROP FOREIGN KEY question_label_fk;
 
@@ -11,5 +12,17 @@ DROP question_label;
 
 DELETE from maintable;
 
+#univariate analysis - description analysis type
+#mean value
+#median value
+#min value
+SELECT MIN(column_name)
+FROM table_name
+WHERE condition;
+#max value
+SELECT country.CountryCode, questioncode.question_code, questioncode.question_label, maintable.answer, MAX(maintable.percentage) AS max_percentage
+FROM maintable, country, questioncode
+WHERE questioncode.question_code = 'fb1_3' AND maintable.answer = 'Very widespread'
+GROUP BY country.CountryCode, questioncode.question_code, questioncode.question_label, maintable.answer;
 
 
