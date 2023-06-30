a4a9884d64a309b2529d8ca6b0a8dde79ef751ca
Kat <128542006+HowToLearnCIn24hours@users.noreply.github.com>
2023-06-30 15:02:40 +0000

    Update analysis code.py

--- python/analysis code.py
+++ python/analysis code.py
@@ -3,14 +3,15 @@ import numpy as np
 import matplotlib as plt
 import seaborn as sns
 plt.style.use('ggplot')
-pd.set_option('display.max_colwidth', None) #Dispaly full questions
-pd.set_option('max_columns', 200)
-#records - where my tables are stored (can I store multipletables in 1 variable? search later)
-records.shape
-records.head(5)
-records.columns
-records.dtypes
-records.describe
+pd.options.display.max_columns = 10
+df = pd.read_csv('C://Users/Me/source/repos/Data-analytics-portfolio-project/source/LGBT_Survey_uniteddata.csv')
+#checking the shape of a dataframe
+df.shape
+df.head(5)
+#checking what columns I have in a table
+df.columns
+#columns types
+df.dtypes
 #check for null values
-records.isna().sum()
+df.isna().sum()
 
