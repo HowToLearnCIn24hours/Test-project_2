5e1ceccafaf7e865d599bd22f685192167a0fec9
Kat <128542006+HowToLearnCIn24hours@users.noreply.github.com>
2023-07-11 16:49:21 +0000

    Update sql\python connector.py

--- python/sql\python connector.py
+++ python/sql\python connector.py
@@ -2,17 +2,22 @@ import mysql.connector
 from mysql.connector import errorcode
 connection = mysql.connector.connect(user='root', password='password', host='localhost', database='lgbt_survey_db')
 cursor = connection.cursor()
-query = ("SELECT maintable.row_id, country.CountryCode, subset.subset, questioncode.question_code, questioncode.question_label, maintable.answer, maintable.percentage FROM maintable, country, questioncode, subset")
-try:
-    cursor.execute(query)
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-       print("Database does not exist")
-    else:
-        print(err)
+query = """
+SELECT maintable.row_id, 
+country.CountryCode, 
+subset.subset, 
+questioncode.question_code, 
+questioncode.question_label, 
+maintable.answer, 
+maintable.percentage 
+FROM maintable, country, questioncode, subset
+"""
+cursor.execute(query)
+results = []
+for i, data in enumerate(cursor):
+    results.append(data)
 
-import numpy as np
+cursor.close()
+connection.close()
 
-query.shape
+df.pulled = pd.DataFrame(results)
